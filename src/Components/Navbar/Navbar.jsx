b54be82e36c78127372ac84fdf9b0ee9ea1229e6
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2023-06-04 16:03:49 +0600

    nav bar created

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,65 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
+import logo from "../../assests/logo.png";
 
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100 container mx-auto">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className=" cursor-pointer normal-case text-xl w-36">
+          <img src={logo} alt="logo" />
+        </a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Link</a>
+            <a href="/">Home</a>
           </li>
           <li>
             <details>
-              <summary>Parent</summary>
+              <summary>About</summary>
+              <ul className="p-2 bg-base-100">
+                <li>
+                  <a>Link 1</a>
+                </li>
+                <li>
+                  <a>Link 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <details>
+              <summary>Pages</summary>
+              <ul className="p-2 bg-base-100">
+                <li>
+                  <a>Link 1</a>
+                </li>
+                <li>
+                  <a>Link 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <details>
+              <summary>Our Services</summary>
+              <ul className="p-2 bg-base-100">
+                <li>
+                  <a>Link 1</a>
+                </li>
+                <li>
+                  <a>Link 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a href="/">Portfolio</a>
+          </li>
+          <li>
+            <details>
+              <summary>Blog</summary>
               <ul className="p-2 bg-base-100">
                 <li>
                   <a>Link 1</a>
@@ -25,6 +71,7 @@ const Navbar = () => {
             </details>
           </li>
         </ul>
+        <FiSearch size={25} />
       </div>
     </div>
   );
