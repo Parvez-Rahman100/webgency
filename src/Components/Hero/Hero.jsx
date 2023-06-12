3a2a7b367c838cbaa671aa1178d1cb8e5701341c
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2023-06-12 15:46:14 +0600

    hero section modified

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import heroRight from "../../assests/hero-right.png";
+import heroBg from "../../assests/heroBg.png";
 import HeroImg from "../../assests/web-design-hero-min.jpg";
 
 const Hero = () => {
   return (
-    <div className="bg-gradient-to-r from-indigo-500 via-  blue-500 to-teal-700 py-40 ">
-      <div className="container mx-auto ">
-        <div className="absolute z-10 top-36 left-1/4">
-          <h1 className=" text-[125px] font-bold italic ">
-            WEB <br />
-            <span className="text-black">DESIGN</span>
-            <br /> AGENCY
-          </h1>
-        </div>
-        <div className="relative flex flex-col  justify-end items-end">
-          <div className="mb-36">
-            <img src={heroRight} alt="hero-right" />
-          </div>
-          <div className=" bg-cover bg-center w-full ">
-            <img src={HeroImg} alt="hero" className="bg-cover bg-center" />
+    <div>
+      <div className="bg-gradient-to-r from-black to-[#0A1B1A] py-40 ">
+        <div
+          style={{
+            backgroundImage: `url(${heroBg})`,
+            backgroundRepeat: "round",
+          }}
+        >
+          <div className="container mx-auto ">
+            <div className="absolute z-10 top-36 left-1/4">
+              <h1 className=" text-[125px] font-bold italic text-white select-none ">
+                WEB <br />
+                <span className="text-white ">DESIGN</span>
+                <br /> AGENCY
+              </h1>
+            </div>
+            <div className="relative flex flex-col  justify-end items-end">
+              <div className="mb-36 select-none">
+                <img src={heroRight} alt="hero-right" />
+              </div>
+              <div className=" bg-cover bg-center w-full select-none ">
+                <img src={HeroImg} alt="hero" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
