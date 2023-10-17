9726f4ae6fb235be29078e247863c09ec986848e
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2023-10-17 21:00:00 +0600

    team members section responsived

--- src/Components/TeamMembers/TeamMembers.jsx
+++ src/Components/TeamMembers/TeamMembers.jsx
@@ -49,97 +49,91 @@ const TeamMembers = () => {
   };
 
   return (
-    <div className="bg-black">
-      <div className="container mx-auto">
-        <p className=" text-emerald-500 flex items-center justify-center mb-6 font-bold text-2xl pt-10">
-          <BsArrowReturnRight /> <span className="ml-4">Team Members</span>
-        </p>
-        <h1 className=" text-6xl font-design mb-10 font-bold italic text-center text-white py-6">
-          Meet Our Professionals Team Members
-        </h1>
-        <Slider {...settings}>
-          <div className="card w-96  shadow-xl cursor-pointer">
-            <figure>
-              <img src={person1} alt="person" className="rounded-xl" />
-            </figure>
-            <div className=" flex justify-around mt-8  items-center text-center">
-              <div>
-                <h2 className="card-title text-2xl text-white mb-2">
-                  Mrs . Bindu
-                </h2>
-                <p className="text-white text-xl">Web Designer</p>
-              </div>
-              <div>
-                <FiArrowUpRight size={30} />
-              </div>
+    <div className="container lg:w-10/12 w-9/12 mx-auto">
+      <p className=" text-emerald-500 flex items-center justify-center mb-6 font-bold text-2xl pt-10">
+        <BsArrowReturnRight /> <span className="ml-4">Team Members</span>
+      </p>
+      <h1 className="lg:text-6xl text-2xl font-design mb-10 font-bold italic text-center text-white py-6">
+        Meet Our Professionals Team Members
+      </h1>
+      <Slider {...settings}>
+        <div className="card w-96  shadow-xl cursor-pointer">
+          <figure>
+            <img src={person1} alt="person" className="rounded-xl w-5/6 " />
+          </figure>
+          <div className=" flex justify-around mt-8  items-center text-center">
+            <div>
+              <h2 className="card-title text-2xl text-white mb-2">
+                Mrs . Bindu
+              </h2>
+              <p className="text-white lg:text-xl text-sm">Web Designer</p>
+            </div>
+            <div>
+              <FiArrowUpRight size={30} />
             </div>
           </div>
-          <div className="card w-96 cursor-pointer ">
-            <figure>
-              <img src={person2} alt="person" className="rounded-xl" />
-            </figure>
-            <div className=" flex justify-around mt-8  items-center text-center">
-              <div>
-                <h2 className="card-title text-2xl text-white mb-2">
-                  Mr. Anik
-                </h2>
-                <p className="text-white text-xl">Web Developer</p>
-              </div>
-              <div>
-                <FiArrowUpRight size={30} />
-              </div>
+        </div>
+        <div className="card w-96 cursor-pointer ">
+          <figure>
+            <img src={person2} alt="person" className="rounded-xl w-5/6 " />
+          </figure>
+          <div className=" flex justify-around mt-8  items-center text-center">
+            <div>
+              <h2 className="card-title text-2xl text-white mb-2">Mr. Anik</h2>
+              <p className="text-white lg:text-xl text-sm">Web Developer</p>
+            </div>
+            <div>
+              <FiArrowUpRight size={30} />
             </div>
           </div>
-          <div className="card w-96 cursor-pointer ">
-            <figure>
-              <img src={person3} alt="person" className="rounded-xl" />
-            </figure>
-            <div className=" flex justify-around mt-8  items-center text-center">
-              <div>
-                <h2 className="card-title text-2xl text-white mb-2">
-                  Mr. Parvez
-                </h2>
-                <p className="text-white text-xl">Web Developer</p>
-              </div>
-              <div>
-                <FiArrowUpRight size={30} />
-              </div>
+        </div>
+        <div className="card w-96 cursor-pointer ">
+          <figure>
+            <img src={person3} alt="person" className="rounded-xl w-5/6" />
+          </figure>
+          <div className=" flex justify-around mt-8  items-center text-center">
+            <div>
+              <h2 className="card-title text-2xl text-white mb-2">
+                Mr. Parvez
+              </h2>
+              <p className="text-white lg:text-xl text-sm">Web Developer</p>
+            </div>
+            <div>
+              <FiArrowUpRight size={30} />
             </div>
           </div>
-          <div className="card w-96 cursor-pointer ">
-            <figure>
-              <img src={person4} alt="person" className="rounded-xl" />
-            </figure>
-            <div className=" flex justify-around mt-8  items-center text-center">
-              <div>
-                <h2 className="card-title text-2xl text-white mb-2">
-                  Mr. Aurko
-                </h2>
-                <p className="text-white text-xl">Project Manager</p>
-              </div>
-              <div>
-                <FiArrowUpRight size={30} />
-              </div>
+        </div>
+        <div className="card w-96 cursor-pointer ">
+          <figure>
+            <img src={person4} alt="person" className="rounded-xl w-5/6 " />
+          </figure>
+          <div className=" flex justify-around mt-8  items-center text-center">
+            <div>
+              <h2 className="card-title text-2xl text-white mb-2">Mr. Aurko</h2>
+              <p className="text-white lg:text-xl text-sm">Project Manager</p>
+            </div>
+            <div>
+              <FiArrowUpRight size={30} />
             </div>
           </div>
-          <div className="card w-96 cursor-pointer ">
-            <figure>
-              <img src={person5} alt="person" className="rounded-xl" />
-            </figure>
-            <div className=" flex justify-around mt-8  items-center text-center">
-              <div>
-                <h2 className="card-title text-2xl text-white mb-2">
-                  Mrs. Nisha
-                </h2>
-                <p className="text-white text-xl">Data Analyzer</p>
-              </div>
-              <div>
-                <FiArrowUpRight size={30} />
-              </div>
+        </div>
+        <div className="card w-96 cursor-pointer ">
+          <figure>
+            <img src={person5} alt="person" className="rounded-xl w-5/6" />
+          </figure>
+          <div className=" flex justify-around mt-8  items-center text-center">
+            <div>
+              <h2 className="card-title text-2xl text-white mb-2">
+                Mrs. Nisha
+              </h2>
+              <p className="text-white lg:text-xl text-sm">Data Analyzer</p>
+            </div>
+            <div>
+              <FiArrowUpRight size={30} />
             </div>
           </div>
-        </Slider>
-      </div>
+        </div>
+      </Slider>
     </div>
   );
 };
