448f6c185517d4180a124eacaa1ac9e988f4d719
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2023-10-17 23:53:27 +0600

    last modification done

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import HeroImg from "../../assests/web-design-hero-min.jpg";
 
 const Hero = () => {
   return (
-    <div>
+    <div className="container mx-auto">
       <div className="bg-gradient-to-tr from-black to-[#0A1B1A] py-40 ">
         <div
           style={{
@@ -13,21 +13,19 @@ const Hero = () => {
             backgroundRepeat: "round",
           }}
         >
-          <div className="container mx-auto ">
-            <div className="absolute z-[1] top-36 left-1/4">
-              <h1 className=" lg:text-[125px] text-[50px] font-bold italic text-white select-none ">
-                WEB <br />
-                <span className="font-new text-emerald-500">DESIGN</span>
-                <br /> AGENCY
-              </h1>
+          <div className="absolute z-[1] top-20 left-1/4">
+            <h1 className=" lg:text-[120px] text-[50px] font-bold italic text-white select-none ">
+              WEB <br />
+              <span className="font-new text-emerald-500">DESIGN</span>
+              <br /> AGENCY
+            </h1>
+          </div>
+          <div className="relative flex-row flex lg:flex-col  justify-end items-end">
+            <div className=" lg:mb-20 mb-32 select-none">
+              <img src={heroRight} alt="hero-right" />
             </div>
-            <div className="relative flex-row flex lg:flex-col  justify-end items-end">
-              <div className="lg:mb-36 mb-32 select-none">
-                <img src={heroRight} alt="hero-right" />
-              </div>
-              <div className="bg-cover bg-center w-full select-none ">
-                <img src={HeroImg} alt="hero" />
-              </div>
+            <div className="bg-cover lg:w-11/12 w-full bg-center mx-auto select-none ">
+              <img src={HeroImg} className=" rounded-xl" alt="hero" />
             </div>
           </div>
         </div>
