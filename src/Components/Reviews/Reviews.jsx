858d0e51ec4eb286b0ce40eec735e8f7c4f99989
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2023-10-17 23:15:20 +0600

    review section responsived

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,29 +1,33 @@
 import React from "react";
-import { BsStarFill, BsStarHalf } from "react-icons/bs";
+import { BsArrowReturnRight, BsStarFill, BsStarHalf } from "react-icons/bs";
 // import {RiDoubleQuotesR} from 'react-icons/ri';
 
 import reviewer from "../../assests/reviewer.jpg";
 
 const Reviews = () => {
   return (
-    <div className=" bg-black pt-32">
-      <div className="flex justify-center flex-col lg:flex-row items-center gap-10  container mx-auto">
-        <div className="lg:w-1/5 w-full ">
-          <div className="flex items-center text-emerald-500">
+    <div className="container mx-auto py-32">
+      <p className=" text-emerald-500 flex items-center justify-center mb-6 font-bold text-2xl py-10">
+        <BsArrowReturnRight />
+        <span className="ml-4">Review From A Customer.</span>
+      </p>
+      <div className="flex justify-center flex-col-reverse lg:flex-row mx-auto items-center gap-10">
+        <div className="lg:w-2/5 w-full mx-auto ">
+          <div className="flex w-10/12 lg:w-full items-center text-emerald-500 mx-auto">
             <BsStarFill />
             <BsStarFill />
             <BsStarFill />
             <BsStarFill />
             <BsStarHalf />
           </div>
-          <p className="mt-5">
+          <p className="mt-5 w-10/12 lg:w-full mx-auto">
             On the other hand, we denounce see with righteous indignation and
             dislike men who are so beguiled and demoralized by the charms of
             pleasure moment.
           </p>
         </div>
-        <div>
-          <img className="w-5/6" src={reviewer} alt="reviewer" />
+        <div className="flex justify-center items-center">
+          <img className="w-3/6" src={reviewer} alt="reviewer" />
         </div>
       </div>
     </div>
